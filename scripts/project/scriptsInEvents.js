9cc346824b5c94556f8b0bbf9d809c62ce9878dc
Matheus N <matheusnaschaves@gmail.com>
2022-07-14 17:14:58 +0200

    run into death

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,19 @@
+var postMsg = ''
+
+function postText(value){
+console.log('iFrame sender: ' + value)
+parent.postMessage(value, "*")
+}
+
+
+const scriptsInEvents = {
+
+		async Hud_Event2_Act19(runtime, localVars)
+		{
+			postText(runtime.globalVars.postMsg)
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
